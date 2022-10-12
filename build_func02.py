b58663a37914a4ecdb617f42e9be4ae26df00bca
Diyorbek <baxshullayevd4@gmail.com>
2022-10-12 23:00:30 +0500

    2

--- build_func02.py
+++ build_func02.py
@@ -1,6 +1,5 @@
 # the task condition is placed in the README file
 #ttps://github.com/codeschool43/Build_in_function_homework#build_func02
 
-expression =3*(7/5 - 9/4)
-answer =expression
+answer =3*((7/5) - (9/4))
 print (answer)  
